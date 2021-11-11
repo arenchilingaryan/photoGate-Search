d23a3a657ec31d6c1c16682b6fc6d1204f1fa649
arenchilingaryan <arenchilingaryan@yandex.ru>
2021-11-11 01:53:04 -0800

    remove unused components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import Button from 'shared/button/button';
 import './App.css';
 
 const App = () => {
   return (
     <div className="div">
-      Zdarove
-      <button role="heading" disabled>
-        Privet
-      </button>
-      <Button />
+      SEARCH
     </div>
   );
 };
